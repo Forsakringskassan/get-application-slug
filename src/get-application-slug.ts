2baca77ae1aca75e02842c4301658ba72e9db0f0
Olof Fredriksson <oloflolof@gmail.com>
2026-09-07 13:30:46 +0000

    feat: getApplicationSelector

--- src/get-application-slug.ts
+++ src/get-application-slug.ts
@@ -11,3 +11,14 @@
 export function getApplicationSlug(name: string): string {
     return name.replaceAll(/^@([^/]+)\//g, "$1--");
 }
+
+/**
+ * Converts an application name into a CSS class selector.
+ *
+ * @param name - The original package name
+ * @returns Returns a CSS class selector derived from the application name.
+ */
+export function getApplicationSelector(name: string): string {
+    const slug = getApplicationSlug(name);
+    return `.${slug}`;
+}
